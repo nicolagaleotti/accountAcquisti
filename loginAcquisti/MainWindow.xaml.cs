f5cb04aa228708995766e94b7d476d3822459d05
Nicola Galeotti <niccio2802@outlook.it>
2020-11-19 09:47:55 +0100

    funzionalità button accedi

--- loginAcquisti/MainWindow.xaml.cs
+++ loginAcquisti/MainWindow.xaml.cs
@@ -23,11 +23,52 @@ namespace loginAcquisti
         public MainWindow()
         {
             InitializeComponent();
+            txtUtente.Focus();
+            txtPrezzo.IsEnabled = false;
+            txtQuantita.IsEnabled = false;
+            cmbProdotto.IsEnabled = false;
+            btnPulisci.IsEnabled = false;
+            btnStampa.IsEnabled = false;
+            ltbRisultato.IsEnabled = false;
+            btnRimuoviSelezione.IsEnabled = false;
         }
 
-        private void StackPanel_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
+        private const string PASSWORD = "ciao";
+        private string[] prodotti = new string[] { "Felpa", "T-Shirt", "Polo", "Pantalone", "Calzini", "Mutande" };
+        private string utente;
 
+        private void btnAccedi_Click(object sender, RoutedEventArgs e)
+        {
+            utente = txtUtente.Text;
+            string pass = txtPassword.Text;
+
+            if(utente !="" && utente != null && pass == PASSWORD)
+            {
+                txtUtente.IsEnabled = false;
+                txtPassword.IsEnabled = false;
+                btnAccedi.IsEnabled = false;
+
+                txtPrezzo.IsEnabled = true;
+                txtQuantita.IsEnabled = true;
+                cmbProdotto.IsEnabled = true;
+                btnPulisci.IsEnabled = true;
+                btnStampa.IsEnabled = true;
+                ltbRisultato.IsEnabled = true;
+                btnRimuoviSelezione.IsEnabled = true;
+            }
+            else if(utente == "" || utente == null)
+            {
+                MessageBox.Show("Inserire un utente valido","", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtUtente.Text = "";
+                txtPassword.Text = "";
+                txtUtente.Focus();
+            }
+            else if(pass != PASSWORD)
+            {
+                MessageBox.Show("Password errata", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                txtPassword.Text = "";
+                txtPassword.Focus();
+            }
         }
     }
 }
